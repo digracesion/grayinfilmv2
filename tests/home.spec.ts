53a21b36e692a6b60662a869d86715767486b26e
Grace Icay <mggicay@gmail.com>
2023-09-05 08:33:59 +0900

    Fixed await for pages on home.spec.ts

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -23,64 +23,64 @@ test.describe('test the static personal webpage', () => {
   test('check home navbar link is redirecting', async () => {
     await page.getByRole('link', { name: 'Home' }).click()
     expect(page).not.toBe('')
-    expect(page.waitForURL(baseURL))
+    await expect(page).toHaveURL(baseURL)
   })
   test('check blog navbar link is redirecting', async () => {
     await page.getByRole('link', { name: 'Blog' }).first().click()
     expect(page).not.toBe('')
-    expect(page.waitForURL(baseURL + 'blog'))
+    await expect(page).toHaveURL(baseURL + 'blog')
   })
   test('check github navbar link is redirecting', async () => {
     const popupPromise = page.waitForEvent('popup')
     await page.getByRole('link', { name: '\'See @grayinfilm on GitHub' }).click()
     const popup = await popupPromise
-    expect(popup.waitForURL(profileURL.github))
+    await expect(popup).toHaveURL(profileURL.github)
   })
   test('check instagram navbar link is redirecting', async () => {
     const popupPromise = page.waitForEvent('popup')
     await page.getByRole('link', { name: '\'See @grayinfilm on Instagram' }).click()
     const popup = await popupPromise
-    expect(popup.waitForURL(profileURL.instagram))
+    await expect(popup).toHaveURL(profileURL.instagram)
   })
   test('check linkedin navbar link is redirecting', async () => {
     const popupPromise = page.waitForEvent('popup')
     await page.getByRole('link', { name: '\'See @grayinfilm on LinkedIn' }).click()
     const popup = await popupPromise
-    expect(popup.waitForURL(profileURL.linkedin))
+    await expect(popup).toHaveURL(profileURL.linkedin)
   })
   test('check linktree navbar link is redirecting', async () => {
     const popupPromise = page.waitForEvent('popup')
     await page.getByRole('link', { name: '\'See @grayinfilm on LinkTree' }).click()
     const popup = await popupPromise
-    expect(popup.waitForURL(profileURL.linktree))
+    await expect(popup).toHaveURL(profileURL.linktree)
   })
   test('check rss navbar link is redirecting', async () => {
     const popupPromise = page.waitForEvent('popup')
     await page.getByRole('link', { name: '\'See @grayinfilm on RSS' }).click()
     const popup = await popupPromise
-    expect(popup.waitForURL(profileURL.rss))
+    await expect(popup).toHaveURL(profileURL.rss)
   })
   test('check support me on kofi link', async () => {
     const popupPromise = page.waitForEvent('popup')
     await page.getByRole('link', { name: 'Ko-Fi' }).click()
     const popup = await popupPromise
-    expect(popup.waitForURL(profileURL.kofi))
+    await expect(popup).toHaveURL(profileURL.kofi)
   })
   test('check support me on hashnode link', async () => {
     const popupPromise = page.waitForEvent('popup')
     await page.getByRole('link', { name: 'Hashnode' }).click()
     const popup = await popupPromise
-    expect(popup.waitForURL(profileURL.hashnode))
+    await expect(popup).toHaveURL(profileURL.hashnode)
   })
   test('check blog redirection link from read my blogposts', async () => {
     await page.getByRole('link', { name: 'Read my blogposts →' }).click()
     expect(page).not.toBe('')
-    expect(page.waitForURL(baseURL + 'blog'))
+    await expect(page).toHaveURL(baseURL + 'blog')
   })
   test('check blog redirection links from go to blog', async () => {
     await page.getByRole('link', { name: 'Go to blog →' }).click()
     expect(page).not.toBe('')
-    expect(page.waitForURL(baseURL + 'blog'))
+    await expect(page).toHaveURL(baseURL + 'blog')
   })
   test.afterAll(async () => {
     context.close()
