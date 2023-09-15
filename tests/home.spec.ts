495ba6183e1a3f67084526a004f5bc11bbafaddd
Grace Icay <mggicay@gmail.com>
2023-09-15 09:17:41 +0900

    Added testomat reporter

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -26,7 +26,7 @@ async function checkLinkRedirection (linkName: string, expectedURL: string) {
   expect(currentURL).toEqual(expectedURL)
 }
 
-test.describe('test the static personal webpage', () => {
+test.describe('test the static personal webpage @Se616e1c6', () => {
   test.beforeAll(async () => {
     browser = await chromium.launch()
     context = await browser.newContext()
@@ -35,43 +35,43 @@ test.describe('test the static personal webpage', () => {
     page = await context.newPage()
     await page.goto(baseURL)
   })
-  test('check title', async () => {
+  test('check title @T8a56309d', async () => {
     expect(await page.title()).toBe('Brutal Theme | Home')
   })
-  test('check home navbar link is redirecting', async () => {
+  test('check home navbar link is redirecting @T3ecb860e', async () => {
     await page.getByRole('link', { name: 'Home' }).click()
     await expect(page).toHaveURL(baseURL)
   })
-  test('check blog navbar link is redirecting', async () => {
+  test('check blog navbar link is redirecting @T8f04f031', async () => {
     await page.getByRole('link', { name: 'Blog' }).first().click()
     await expect(page).toHaveURL(blogURL)
   })
-  test('check github navbar link is redirecting', async () => {
+  test('check github navbar link is redirecting @T607a0dbd', async () => {
     void checkLinkRedirection('\'See @grayinfilm on GitHub', profileURL.github)
   })
-  test('check instagram navbar link is redirecting', async () => {
+  test('check instagram navbar link is redirecting @Tdfa6a28b', async () => {
     void checkLinkRedirection('\'See @grayinfilm on Instagram', profileURL.instagram)
   })
-  test('check linkedin navbar link is redirecting', async () => {
+  test('check linkedin navbar link is redirecting @T5369c048', async () => {
     void checkLinkRedirection('\'See @grayinfilm on LinkedIn', profileURL.linkedin)
   })
-  test('check links navbar link is redirecting', async () => {
+  test('check links navbar link is redirecting @T07c68866', async () => {
     void checkLinkRedirection('\'See @grayinfilm on BioDrop', profileURL.links)
   })
-  test('check rss navbar link is redirecting', async () => {
+  test('check rss navbar link is redirecting @T411a8b08', async () => {
     void checkLinkRedirection('\'See @grayinfilm on RSS', profileURL.rss)
   })
-  test('check support me on kofi link', async () => {
+  test('check support me on kofi link @T2a975061', async () => {
     void checkLinkRedirection('Ko-Fi', profileURL.kofi)
   })
-  test('check support me on hashnode link', async () => {
+  test('check support me on hashnode link @Teba88b3b', async () => {
     void checkLinkRedirection('Hashnode', profileURL.hashnode)
   })
-  test('check blog redirection link from read my blogposts', async () => {
+  test('check blog redirection link from read my blogposts @T66f61bb5', async () => {
     await page.getByRole('link', { name: 'Read my blogposts →' }).click()
     await expect(page).toHaveURL(blogURL)
   })
-  test('check blog redirection links from go to blog', async () => {
+  test('check blog redirection links from go to blog @Tbf3a8417', async () => {
     await page.getByRole('link', { name: 'Go to blog →' }).click()
     await expect(page).toHaveURL(blogURL)
   })
